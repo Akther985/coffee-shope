92d9c95f61c3815ab7e8ab1a710c4eb6648943f3
Akther985 <bd.bokul1994@gmail.com>
2024-03-13 22:35:08 +0600

    dextop vew

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -54,6 +54,44 @@
             infinigall: true
           });
 
+          $('.blog').slick({
+            dots:false,
+            infinite: true,
+            autoplay:true,
+            arrows:true,
+            speed: 300,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                  infinite: true,
+                  dots:false,
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+              
+            ]
+          });    
+          
+         
+
           
 
           
